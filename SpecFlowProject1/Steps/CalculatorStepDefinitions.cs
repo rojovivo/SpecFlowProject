2e2b1731a57fbd7e18f1d5991025603e8dc431d3
rojovivo <josefrancisco.ibarra@gmail.com>
2020-08-26 14:15:59 +0200

    minor changes

--- SpecFlowProject1/Steps/CalculatorStepDefinitions.cs
+++ SpecFlowProject1/Steps/CalculatorStepDefinitions.cs
@@ -8,11 +8,7 @@ namespace SpecFlowProject1.Steps
     public sealed class CalculatorStepDefinitions
     {
         private readonly Calculator _calculator = new Calculator();
-        private int _result;
-
-
-        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
-
+        private int _result;        
         private readonly ScenarioContext _scenarioContext;
 
         public CalculatorStepDefinitions(ScenarioContext scenarioContext)
@@ -23,32 +19,18 @@ namespace SpecFlowProject1.Steps
         [Given("the first number is (.*)")]
         public void GivenTheFirstNumberIs(int number)
         {
-            //TODO: implement arrange (precondition) logic
-            // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata
-            // To use the multiline text or the table argument of the scenario,
-            // additional string/Table parameters can be defined on the step definition
-            // method. 
-
             _calculator.FirstNumber = number;
         }
 
         [Given("the second number is (.*)")]
         public void GivenTheSecondNumberIs(int number)
         {
-            //TODO: implement arrange (precondition) logic
-            // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata
-            // To use the multiline text or the table argument of the scenario,
-            // additional string/Table parameters can be defined on the step definition
-            // method. 
-
             _calculator.SecondNumber = number;
         }
 
         [When("the two numbers are added")]
         public void WhenTheTwoNumbersAreAdded()
         {
-            //TODO: implement act (action) logic
-
             _result = _calculator.Add();
         }
 
